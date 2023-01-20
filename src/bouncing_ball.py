e8e74142d7aed249efeae788c9c53c77715f4776
Alberto <albertorondang@gmail.com>
2023-01-20 15:52:35 +0100

    WIP creating Match class.

--- src/bouncing_ball.py
+++ src/bouncing_ball.py
@@ -12,27 +12,16 @@ def bounce():
     size = (700, 400)
     GRAY = (110, 110, 110)
     screen = pygame.display.set_mode(size)
-    margin_to_border = 30
-    paddle_size = (50, 100)
-
     screen_saver = ScreenSaver(screen=screen)
-    paddle_left = Paddle(left=margin_to_border, top=int(size[1] / 2 - paddle_size[1] / 2), width=paddle_size[0], height=paddle_size[1], color=(0, 0, 0), speed=8, screen=screen)
-    paddle_right = Paddle(left=size[0] - margin_to_border - paddle_size[0], top=int(size[1]/ 2 - paddle_size[1] / 2), width=paddle_size[0], height=paddle_size[1], color=(255, 255, 255), speed=8, screen=screen)
     while True:
         events = pygame.event.get()
         screen_saver.update(events)
         for event in events:
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 sys.exit()
-            if pygame.key.get_pressed()[pygame.K_w]:
-                paddle_left.up()
-            if pygame.key.get_pressed()[pygame.K_s]:
-                paddle_left.down()
         if screen_saver.active():
             screen_saver.display()
         else:
             screen.fill(GRAY)
-            paddle_left.draw()
-            paddle_right.draw()
         pygame.display.flip()
         time.sleep(0.05)
--- src/match.py
+++ src/match.py
@@ -2,12 +2,18 @@ from typing import List
 
 import pygame.event
 
+from src.paddle import Paddle
+
 
 class Match:
     # TODO: Add paddles, display function and update function.
 
-    def __int__(self):
-        pass
+    def __int__(self, screen: pygame.Surface, paddle_size=(50, 100), margin_to_border=30):
+        self._left_paddle = Paddle(left=margin_to_border, top=int(screen.get_size()[1] / 2 - paddle_size[1] / 2),
+                                   width=paddle_size[0], height=paddle_size[1], color=(0, 0, 0), speed=8, screen=screen)
+        self._right_paddle = Paddle(left=screen.get_size()[0] - margin_to_border - paddle_size[0],
+                                    top=int(screen.get_size()[1]/ 2 - paddle_size[1] / 2), width=paddle_size[0],
+                                    height=paddle_size[1], color=(255, 255, 255), speed=8, screen=screen)
 
     def update(self, events: List[pygame.event.Event]):
         """ Updates the state with the given events """
