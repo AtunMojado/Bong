b7e725e9834101ca78047102a747a5a811a68ff5
Alberto <albertorondang@gmail.com>
2023-01-21 20:09:32 +0100

    WIP creating Match class.

--- src/match.py
+++ src/match.py
@@ -13,14 +13,14 @@ class Match:
     # TODO: Add paddles, display function and update function.
 
     def __init__(self, screen: pygame.Surface, paddle_size: Tuple[int, int] = (50, 100), margin_to_border: int = 30,
-                 screen_color: Tuple[int, int, int] = (132, 45, 76), ball_position: Tuple[int, int] = (350, 200)):
+                 screen_color: Tuple[int, int, int] = (32, 155, 93), ball_position: Tuple[int, int] = (350, 200)):
         self._screen = screen
         self._left_paddle = Paddle(left=margin_to_border, top=int(screen.get_size()[1] / 2 - paddle_size[1] / 2),
                                    width=paddle_size[0], height=paddle_size[1], color=(0, 0, 0), speed=8, screen=screen)
         self._right_paddle = Paddle(left=screen.get_size()[0] - margin_to_border - paddle_size[0],
                                     top=int(screen.get_size()[1] / 2 - paddle_size[1] / 2), width=paddle_size[0],
                                     height=paddle_size[1], color=(255, 255, 255), speed=8, screen=screen)
-        self._ball = Ball(color=(123, 0, 0), radius=20, center= ball_position, speed=13, screen=screen)
+        self._ball = Ball(color=(0, 15, 150), radius=20, center= ball_position, speed=13, screen=screen)
         self._screen_color = screen_color
 
     def update(self, events: List[pygame.event.Event]):
