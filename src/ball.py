b97b2b8873a98c19665b47f2705afb85a693c80e
Alberto <albertorondang@gmail.com>
2023-01-23 14:42:35 +0100

    WIP creating Match class.

--- src/ball.py
+++ src/ball.py
@@ -2,9 +2,10 @@ from typing import Tuple, List
 import pygame
 
 
+
 class Ball:
 
-    def __init__(self, color: Tuple[int, int, int], center: Tuple[int, int], radius: int, speed: int, screen: pygame.Surface):
+    def __init__(self, screen: pygame.Surface, color: Tuple[int, int, int], center: Tuple[int, int], radius: int, speed: Tuple[int, int]):
         """Circle that goes all over the screen and collides with the paddles or score goals"""
         self._screen = screen
         self._speed = speed
@@ -12,14 +13,24 @@ class Ball:
         self._radius = radius
         self._center = center
 
+
     def draw(self):
         pygame.draw.circle(self._screen, self._color, self._center, self._radius)
 
-    def change_color(self, color):
-        self._color = color
+    def movement(self):
+        speed = self._speed
+        ball = pygame.draw.circle(self._screen, self._color, self._center, self._radius)
 
-    #def movement(self):
-        #if self._ball.top > 0:
-            #self._ball.move(0, -self._speed)
-        #if self._ball.bottom < self._screen.get_size()[1]:
-            #self._ball = self._ball.move(0, self._speed)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/match.py
+++ src/match.py
@@ -13,14 +13,15 @@ class Match:
     # TODO: Add paddles, display function and update function.
 
     def __init__(self, screen: pygame.Surface, paddle_size: Tuple[int, int] = (50, 100), margin_to_border: int = 30,
-                 screen_color: Tuple[int, int, int] = (32, 155, 93), ball_position: Tuple[int, int] = (350, 200)):
+                 screen_color: Tuple[int, int, int] = (32, 155, 93), ball_position: Tuple[int, int] = (350, 200),
+                 ball_speed: Tuple[int, int] = (8, 8)):
         self._screen = screen
         self._left_paddle = Paddle(left=margin_to_border, top=int(screen.get_size()[1] / 2 - paddle_size[1] / 2),
                                    width=paddle_size[0], height=paddle_size[1], color=(0, 0, 0), speed=8, screen=screen)
         self._right_paddle = Paddle(left=screen.get_size()[0] - margin_to_border - paddle_size[0],
                                     top=int(screen.get_size()[1] / 2 - paddle_size[1] / 2), width=paddle_size[0],
                                     height=paddle_size[1], color=(255, 255, 255), speed=8, screen=screen)
-        self._ball = Ball(color=(0, 15, 150), radius=20, center= ball_position, speed=13, screen=screen)
+        self._ball = Ball(color=(0, 15, 150), radius=15, center= ball_position, speed= ball_speed, screen=screen)
         self._screen_color = screen_color
 
     def update(self, events: List[pygame.event.Event]):
@@ -35,6 +36,8 @@ class Match:
                 self._right_paddle.up()
             if keys[pygame.K_m]:
                 self._right_paddle.down()
+            if keys[pygame.K_SPACE]:
+                self._ball.movement()
 
     def display(self):
         """ Displays the current match """
@@ -42,3 +45,4 @@ class Match:
         self._left_paddle.draw()
         self._right_paddle.draw()
         self._ball.draw()
+        self._ball.movement()
